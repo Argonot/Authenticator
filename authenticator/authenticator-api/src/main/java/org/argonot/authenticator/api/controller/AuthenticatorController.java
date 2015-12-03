182374ce98f619bbec8e829a5d9c7847ac2bdec0
mairouche <meidi.airouche@gmail.com>
2015-12-03 21:35:03 +0100

    Suppression de session non utilisée

--- authenticator/authenticator-api/src/main/java/org/argonot/authenticator/api/controller/AuthenticatorController.java
+++ authenticator/authenticator-api/src/main/java/org/argonot/authenticator/api/controller/AuthenticatorController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * <b>AuthentificatorController</b> expose every RESTFUL methods that can be called relative to an authentification
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.SessionAttributes;
  *
  */
 @Controller
-@SessionAttributes({"userSession"})
 @RequestMapping("/authenticator")
 public class AuthenticatorController {
 
