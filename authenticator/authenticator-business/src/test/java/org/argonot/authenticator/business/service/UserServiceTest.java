c9d199c32fa90c0e42b521e9be0eeae21c82f9ec
mairouche <meidi.airouche@gmail.com>
2015-12-27 22:48:18 +0100

    Correction des tests

--- authenticator/authenticator-business/src/test/java/org/argonot/authenticator/business/service/UserServiceTest.java
+++ authenticator/authenticator-business/src/test/java/org/argonot/authenticator/business/service/UserServiceTest.java
@@ -26,7 +26,7 @@ public class UserServiceTest extends AbstractBusinessSpringContextTest {
     private static final String UPDATE_USER_NEW_SURNAME = "test";
     private static final String EXISTING_USER_EMAIL = "meidi.airouche@gmail.com";
     private static final long LOCKED_USER_ID = 2L;
-    private static final long USER_TO_REMOVE_ID = 5L;
+    private static final long USER_TO_REMOVE_ID = 4L;
     private static final long UNKNOWN_USER_ID = 9999L;
     private static final String FORGE_APP_AUID = "FRG";
     private static final String APP_AUID_WITH_NO_AUTH_FOR_USER = "TKP";
@@ -70,46 +70,45 @@ public class UserServiceTest extends AbstractBusinessSpringContextTest {
     public void testRemovedUserDoesntExistsAnymore() {
         assertTrue(userService.removeUser(USER_TO_REMOVE_ID));
     }
-    
+
     @Test
     public void testRemoveUnknownUser() {
         assertFalse(userService.removeUser(UNKNOWN_USER_ID));
     }
-   
-    // Test de merde
+
     @Test
     public void testUnlockUser() {
         userService.unlockUser(LOCKED_USER_ID);
         assertFalse(userService.find(LOCKED_USER_ID).isLocked());
     }
-    
+
     @Test
     public void testCreateUser() {
         assertNotNull(userService.create(newUser));
     }
-    
+
     @Test
     public void testSubscribeUser() {
         assertNotNull(userService.subscribe(subscribingUser, FORGE_APP_AUID, USER_ROLE_RUID));
     }
-    
+
     @Test
     public void testSubscribeUserAlreadyExistToNewApp() {
         subscribingUser.setEmail(EXISTING_USER_EMAIL);
         assertNotNull(userService.subscribe(subscribingUser, APP_AUID_WITH_NO_AUTH_FOR_USER, USER_ROLE_RUID));
     }
-    
+
     @Test
     public void testSubscribeUserAlreadyExistsWithAuthorization() {
         subscribingUser.setEmail(EXISTING_USER_EMAIL);
         assertNull(userService.subscribe(subscribingUser, FORGE_APP_AUID, ADMIN_ROLE_RUID));
     }
-    
+
     @Test
     public void testUpdateUser() {
         assertEquals(UPDATE_USER_NEW_SURNAME, userService.update(updateUser, UPDATE_USER_ID).getSurname());
     }
-    
+
     @Test
     public void testUpdateUnexistingUser() {
         assertNull(userService.update(updateUser, UNKNOWN_USER_ID));
