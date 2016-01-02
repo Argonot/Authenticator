0fb802214a9422e9c82a00e5e0c321db6a8cee43
mairouche <meidi.airouche@gmail.com>
2016-01-02 11:49:25 +0100

    Ajout de tests unitaires

--- authenticator/authenticator-api/src/test/java/org/argonot/authenticator/api/controller/UserControllerTest.java
+++ authenticator/authenticator-api/src/test/java/org/argonot/authenticator/api/controller/UserControllerTest.java
@@ -15,11 +15,16 @@ public class UserControllerTest extends AbstractControllerTest {
     private static final String USER_PASSWORD = "Test1.";
     private static final String AUID = "FRG";
     private static final String RUID = "USR";
+    private static final String UPDATER_EMAIL = "updater@gmail.com";
+    private static final String UPDATER_NAME = "updaterName";
+    private static final String UPDATER_SURNAME = "updaterSurname";
     private static final String HTTP_POST_METHOD = "POST";
     private static final String SUBSCRIBE_ROUTE = "/users/subscribe";
+    private static final String UPDATE_ROUTE = "/users/update/8";
 
     private static final ObjectMapper jsonMapper = new ObjectMapper();
     private static final CredentialsVO subscriberCredentials = new CredentialsVO();
+    private static final CredentialsVO updaterCredentials = new CredentialsVO();
     
     @Before
     public void setUpTests() {
@@ -29,12 +34,25 @@ public class UserControllerTest extends AbstractControllerTest {
         subscriberCredentials.setPassword(USER_PASSWORD);
         subscriberCredentials.setName(SUBSCRIBER_NAME);
         subscriberCredentials.setSurname(SUBSCRIBER_SURNAME);
+
+        updaterCredentials.setAuid(AUID);
+        updaterCredentials.setRuid(RUID);
+        updaterCredentials.setEmail(UPDATER_EMAIL);
+        updaterCredentials.setPassword(USER_PASSWORD);
+        updaterCredentials.setName(UPDATER_NAME);
+        updaterCredentials.setSurname(UPDATER_SURNAME);
     }
 
     @Test
     public void testSubscribeUser() throws Exception{
         this.request.setContent(jsonMapper.writeValueAsBytes(subscriberCredentials));
         this.testJsonObjectRoute(SUBSCRIBE_ROUTE, HTTP_POST_METHOD);
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+        this.request.setContent(jsonMapper.writeValueAsBytes(updaterCredentials));
+        this.testJsonObjectRoute(UPDATE_ROUTE, HTTP_POST_METHOD);
     }
 
 }
